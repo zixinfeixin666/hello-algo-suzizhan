7738fae1202877a3f84bbd6ad311579df62d757c
zixinfeixin666 <1781935376@qq.com>
2023-09-12 01:38:15 +0800

    两道回溯算法已完成

--- codes/java/chapter_backtracking/preorder_traversal_ii_compact.java
+++ codes/java/chapter_backtracking/preorder_traversal_ii_compact.java
@@ -13,40 +13,36 @@ public class preorder_traversal_ii_compact {
     static List<TreeNode> path;
     static List<List<TreeNode>> res;
 
-    /* 前序遍历：例题二 */
-    static void preOrder(TreeNode root) {
-        if (root == null) {
+    static void preOder(TreeNode root){
+        if(root == null){
             return;
         }
-        // 尝试
         path.add(root);
-        if (root.val == 7) {
-            // 记录解
+        if(root.val == 7){
             res.add(new ArrayList<>(path));
         }
-        preOrder(root.left);
-        preOrder(root.right);
-        // 回退
+        preOder(root.left);
+        preOder(root.right);
         path.remove(path.size() - 1);
     }
 
     public static void main(String[] args) {
-        TreeNode root = TreeNode.listToTree(Arrays.asList(1, 7, 3, 4, 5, 6, 7));
-        System.out.println("\n初始化二叉树");
+        TreeNode root = TreeNode.listToTree(Arrays.asList(1,7,3,4,5,6,7));  
+        System.out.println("初始化二叉树");
         PrintUtil.printTree(root);
 
-        // 前序遍历
         path = new ArrayList<>();
         res = new ArrayList<>();
-        preOrder(root);
+        preOder(root);
 
         System.out.println("\n输出所有根节点到节点 7 的路径");
-        for (List<TreeNode> path : res) {
-            List<Integer> vals = new ArrayList<>();
-            for (TreeNode node : path) {
+        for(List<TreeNode> path : res){
+            ArrayList<Integer> vals = new ArrayList<>();
+            for(TreeNode node : path){
                 vals.add(node.val);
             }
             System.out.println(vals);
         }
+
     }
 }
